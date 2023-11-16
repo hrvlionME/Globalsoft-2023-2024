33fbeca919474e0950ab5c6b19a040c8e61a8819
Mario <mariokozul24@gmail.com>
2023-11-16 23:11:42 +0100

    added route for root endpoint

---
+++ backend/routes/main.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import * as mainController from '../controllers/main.js';
+
+export const mainRouter = express.Router();
+
+//routes
+mainRouter.get('/', mainController.rootEndpoint);
+
