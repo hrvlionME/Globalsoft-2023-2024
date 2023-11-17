74ae6bf004c9d6ce1252b7743040434d540a6617
hrvlionme <hrvlionme@gmail.com>
2023-11-17 11:32:30 +0100

    Add/Remove button working now

--- frontend/src/components/CreateGroup/UsersList/UsersList.jsx
+++ frontend/src/components/CreateGroup/UsersList/UsersList.jsx
@@ -1,16 +1,31 @@
 import styles from './UsersList.module.css';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 
 export default function UsersList(){
     //Niz naziv usera koji su već dodani za stvaranje nove grupe
-    const userArray = ["Me", "Test1", "Test2", "Test3", "Test4"];
+    const userArray = ["Me", "AAA"];
     const [users, setUsers] = useState(userArray); 
-    
 
+    //Niz usera koji nisu dodani
+    const user2Array = ["Test1", "Test2", "Test3", "Test4"];
+    const [users2, setUsers2] = useState(user2Array);
     
-    function removeUser(){
+    /* useEffect(() => {
+        fetch("http://localhost:8000/")
+          .then((res) => res.json())
+          .then((data) => console.log(data.message)) //dodati data u niz users2
+      }, []);
+    */
 
+    function removeUser(userToDelete){
+        setUsers((current) => (current.filter((user) => {return user !== userToDelete})));
+        setUsers2((current) => [...current, userToDelete]);
+    }
+
+    function addUser(userToAdd){
+        setUsers2((current) => (current.filter((user) => {return user !== userToAdd})));
+        setUsers((current) => [...current, userToAdd]);
     }
 
     return (
@@ -20,17 +35,17 @@ export default function UsersList(){
                     // Setting "index" as key because name and age can be repeated, It will be better if you assign uniqe id as key
                     <li className={styles.user} key={index}>
                         <span>{user}</span>
-                        <button className={styles.Remove}>X</button>  {/*treba ubacit X kao mogucnost brisanja ubacenog korisnika*/}
+                        <button className={styles.Remove} onClick={() => removeUser(user)}>X</button>  {/*treba ubacit X kao mogucnost brisanja ubacenog korisnika*/}
                     </li>
                 ))}
             </ul>
             <ul>  
-                {users.map((user, index) => (       
+                {users2.map((user, index) => (       
                 //Ovdje bi trebao ic niz usera koji nije u gornjem nizu, odnosno ako smo dodali usera gore treba ga izbacit dole
                 <li className={styles.userInfo} key={index}>
                     <img src="" alt="" />  {/*profilna za usera*/}
                     <span>{user}</span>
-                    <button className={styles.Add}>+</button>
+                    <button className={styles.Add} onClick={() => addUser(user)}>+</button>
                 </li>
                 ))}
 
