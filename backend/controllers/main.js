4efc3102bb9e17cf27ec8c930aaa0c318e92cb70
Mario <mariokozul24@gmail.com>
2023-11-19 23:05:28 +0100

    function for handling /createNewGroupChat endpoint

--- backend/controllers/main.js
+++ backend/controllers/main.js
@@ -6,10 +6,9 @@ export const rootEndpoint = async (req,res) => {
 }
 
 export const createNewGroupChat = async (req,res) => {
-    //middleware, validation, authentication and authorization will be added later
-    const participants = req.participants;
-    const chatName = req.name;
-    const isSuccess = await db.insertNewGroupChatData(participants, chatName);
-    if(isSuccess) return res.json({message: 'Groupchat created successfully'},200);
-    return res.json({message: 'Error occured'},500);
+    const usersIds = [...req.body.participants];
+    const name = req.body.name;
+    const isSuccess = await db.insertNewGroupChatData(usersIds, name);
+    if(isSuccess) return res.status(200).json({message: 'Groupchat created successfully'});
+    return res.status(500).json({message: 'Error occured'});
 }
