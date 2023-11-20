8513d475c990cd9425b841b6b075e929106d889b
markoduspara <m.duspara@globalsoft.ba>
2023-11-20 15:20:32 +0100

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,14 +3,16 @@ import { config } from 'dotenv';
 
 config();
 
-const dbConn = mysql.createPool({
-  host: process.env.HOST,
-  user: process.env.USER,
-  password: process.env.PASSWORD,
-  database: process.env.DATABASE,
-}).promise();
+const dbConn = mysql
+  .createPool({
+    host: process.env.HOST,
+    user: process.env.DB_USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+  })
+  .promise();
 
-export async function getInfo(){
+export async function getInfo() {
   const [rows] = await dbConn.query(`
   SELECT *
   FROM users;
@@ -18,18 +20,21 @@ export async function getInfo(){
   return rows;
 }
 
-export async function insertNewGroupChatData(participantsInfo, chatName){
+export async function insertNewGroupChatData(participantsInfo, chatName) {
   const query1 = `INSERT INTO chat (name) VALUES (?);`;
   const query2 = `INSERT INTO participants (user_id, chat_id) VALUES (?, ?);`;
   const results = [];
-  try{
+  try {
     const [result1] = await dbConn.query(query1, chatName);
-    for(let i = 0; i < participantsInfo.length; i++){
-      const [result2] = await dbConn.query(query2, [participantsInfo[i], result1.insertId]);
+    for (let i = 0; i < participantsInfo.length; i++) {
+      const [result2] = await dbConn.query(query2, [
+        participantsInfo[i],
+        result1.insertId,
+      ]);
       results.push(result2.insertId);
     }
     return results;
-  } catch(err) {
+  } catch (err) {
     console.log(err);
   }
 }
