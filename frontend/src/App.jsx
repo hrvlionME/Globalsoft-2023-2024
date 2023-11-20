d393226f41fbe285e31103d57ea5f0dcd71466ce
Josip <josip.bosnjak@fpmoz.sum.ba>
2023-11-20 11:21:34 +0100

    unified css in index.css & edited Dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
-import Dashboard from './components/Dashboard/Dashboard.jsx';
+// App.jsx
+import React from 'react';
+import Dashboard from './components/Dashboard/Dashboard';
+import backgroundImage from './assets/bg.jpg';
+import './App.module.css'; // Import your CSS file if using external styles
 
-function App() {
+const App = () => {
+  const styles = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    // Other background properties
+  };
+
   return (
-    <>
+    <div style={styles}>
       <Dashboard />
-    </>
+      {/* Other components and content go here */}
+    </div>
   );
-}
+};
 
 export default App;
--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -2,59 +2,73 @@ import PropTypes from 'prop-types'; // Import PropTypes
 import { useState } from 'react';
 import CreateGroupButton from '../CreateGroup/CreateGroupButton/CreateGroupButton';
 import './Dashboard.css';
+import profilePhoto from '../../assets/guitar.png';
+import searchPhoto from '../../assets/search.png';
 
 const ChatList = ({ onSelect }) => {
-  const chatData = [
-    { id: 1, name: 'Chat 1' },
-    { id: 2, name: 'Chat 2' },
-    { id: 3, name: 'Chat 3' },
-  ];
+  const [numberOfChats, setNumberOfChats] = useState(3);
 
-  return (
-    <ul className="chat-list-container">
-      {chatData.map((chat) => (
-        <li key={chat.id} onClick={() => onSelect(chat.id)}>
-          {chat.name}
-        </li>
-      ))}
-    </ul>
-  );
-};
-
-ChatList.propTypes = {
-  onSelect: PropTypes.func.isRequired,
-};
-
-const ChatView = ({ chatId }) => {
-  const chatData = {
-    1: { id: 1, name: 'Chat 1', messages: ['Message 1', 'Message 2'] },
-    2: { id: 2, name: 'Chat 2', messages: ['Message 3', 'Message 4'] },
-    3: { id: 3, name: 'Chat 3', messages: ['Message 5', 'Message 6'] },
+  const addNewChat = () => {
+    const newNumberOfChats = numberOfChats + 1;
+    setNumberOfChats(newNumberOfChats);
+    onSelect(newNumberOfChats);
   };
 
-  const selectedChat = chatData[chatId];
-
   return (
-    <div className="chat-view-container">
-      <h2>{selectedChat ? selectedChat.name : ''}</h2>
-      <ul>
-        {selectedChat &&
-          selectedChat.messages.map((message, index) => (
-            <li key={index}>{message}</li>
-          ))}
-      </ul>
+    <div className="chat-list-container">
+      {Array.from({ length: numberOfChats }, (_, index) => (
+        <div
+          key={index}
+          onClick={() => onSelect(index + 1)}
+          className="conversation"
+        >
+          Chat {index + 1}
+        </div>
+      ))}
+      <button className="add-chat-button" onClick={addNewChat}>
+        +
+      </button>
     </div>
   );
 };
 
-ChatView.propTypes = {
-  chatId: PropTypes.number,
+const ChatView = ({ chatId }) => {
+  const selectedChat = chatId ? `Chat ${chatId}` : '';
+
+  return (
+    <div className="chat-view">
+      <div className="chat-view-title">
+        <h2>{selectedChat}</h2>
+      </div>
+      <div className="chat-view-messages">
+        <h2>Messages for {selectedChat}</h2>
+      </div>
+      <div className="chat-view-send">
+        <p>Send message...</p>
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  const [selectedChat, setSelectedChat] = useState(null);
+
+  const handleChatSelect = (chatId) => {
+    setSelectedChat(chatId);
+  };
+
+  return (
+    <div className="app-container">
+      <ChatList onSelect={handleChatSelect} />
+      <ChatView chatId={selectedChat} />
+    </div>
+  );
 };
 
 const UserInfo = () => {
   const userData = {
-    username: 'JohnDoe',
-    email: 'john.doe@example.com',
+    username: 'Backend24seven',
+    email: 'puspauza@mysequel.com',
   };
 
   const handleLogout = () => {
@@ -64,19 +78,23 @@ const UserInfo = () => {
   return (
     <div className="user-info">
       <div className="profile-container">
-        <img src="/avatar.jpg" alt="Profile" className="profile-photo" />
+        <img src={profilePhoto} alt="Profile" className="profile-photo" />
         <div className="user-details">
           <h1>{userData.username}</h1>
-          <p>Email: {userData.email}</p>
+          <p>{userData.email}</p>
         </div>
       </div>
       <div className="buttons">
-          <div className="create-button">
-            <CreateGroupButton />
-          </div>
-          <button onClick={handleLogout} className="logout-button">
-        Logout
-      </button>
+        <div className="create-button">
+          <CreateGroupButton />
+        </div>
+        <div className="search">
+          <p>Search...</p>
+          <img src={searchPhoto} alt="Search" className="search-photo" />
+        </div>
+        <button onClick={handleLogout} className="button">
+          Button
+        </button>
       </div>
     </div>
   );
@@ -98,12 +116,10 @@ const Dashboard = () => {
         <div className="chat-list">
           <ChatList onSelect={handleChatSelect} />
         </div>
-        <div className="chat-view">
-          <ChatView chatId={selectedChat} />
-        </div>
+        <ChatView chatId={selectedChat} />
       </div>
       <footer>
-        <p>Contact Info: your@email.com</p>
+        <h4>Limited Edition</h4>
       </footer>
     </div>
   );
