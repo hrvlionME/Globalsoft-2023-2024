32f614f3a94715feb7b923153f782de2d20e37e5
Marko <marko@Markos-MacBook-Air.local>
2023-11-26 16:39:58 +0100

    add chat component

---
+++ frontend/src/components/ChatView/ChatInfo.jsx
@@ -0,0 +1,19 @@
+import PropTypes from "prop-types";
+import "./ChatInfo.css";
+
+export default function ChatInfo() {
+  ChatInfo.propTypes = {
+    chatID: PropTypes.number,
+  };
+
+  return (
+    <div className="chat-info">
+      <img
+        className="avatar"
+        src="https://s.yimg.com/ny/api/res/1.2/ucHuV6bXXCrbJuvd1WdEEA--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTM2MA--/https://media.zenfs.com/en/fox_business_text_367/57bdb2b1d3617f7322b2e729d1fe69be"
+        alt="avatar"
+      />
+      <div className="chat-info-name">Chat name</div>
+    </div>
+  );
+}
