f2e7352c1bdee2c812df31e9b99bf21b40a26ed0
markoduspara <m.duspara@globalsoft.ba>
2023-12-15 15:08:18 +0100

    login bool

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,12 @@
 // App.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import Dashboard from './components/Dashboard/Dashboard';
+
 import './App.module.css'; // Import your CSS file if using external styles
+import Login from './components/Login/Login';
 
 const App = () => {
+  const [isLoggedIn, setisLoggedIn] = useState(false);
   const styles = {
     backgroundColor: 'rgb(23, 26, 36)',
     backgroundSize: 'cover',
@@ -13,7 +16,7 @@ const App = () => {
 
   return (
     <div style={styles}>
-      <Dashboard />
+      {isLoggedIn ? <Dashboard /> : <Login />}
 
       {/* Other components and content go here */}
     </div>
