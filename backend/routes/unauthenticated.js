2ccaba78d68a13f0a5bfcca7ed9d68d96bddfb59
Mario <mariokozul24@gmail.com>
2024-01-15 03:39:24 +0100

    logout, refreshToken and test route

--- backend/routes/unauthenticated.js
+++ backend/routes/unauthenticated.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import * as mainController from '../controllers/main.js';
+import { refreshAuth } from '../middleware/refreshTokenMiddleware.js';
 
 export const router = express.Router();
 
 router.get('/', mainController.rootEndpoint);
 router.post('/login', mainController.login);
 router.route('/register').post(mainController.registerUser); // Register a new user
+router.get('/refreshToken', refreshAuth, mainController.refreshToken);
